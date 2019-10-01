2e6c3c603e14033817ddd10034388699479a42f0
tumis <tumisanejoseph@gmail.com>
2019-10-01 21:17:59 +0200

    Second commit attempt Spring MVC skeleton and Hibernate integration with dao

---
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -0,0 +1,81 @@
+package com.emusicstore.controller;
+
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Le on 1/2/2016.
+ */
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model) {
+        List<Product> products = productDao.getAllProducts();
+        model.addAttribute("products", products);
+
+        return "productList";
+    }
+
+    @RequestMapping("/productList/viewProduct/{productId}")
+    public String viewProduct(@PathVariable String productId, Model model) throws IOException{
+
+        Product product = productDao.getProductById(productId);
+        model.addAttribute(product);
+
+        return "viewProduct";
+    }
+
+    @RequestMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @RequestMapping("/admin/productInventory")
+    public String productInventory(Model model) {
+        List<Product> products = productDao.getAllProducts();
+        model.addAttribute("products", products);
+
+        return "productInventory";
+    }
+
+
+    @RequestMapping("/admin/productInventory/addProduct")
+    public String addProduct(Model model) {
+        Product product = new Product();
+        product.setProductCategory("instrument");
+        product.setProductCondition("new");
+        product.setProductStatus("active");
+
+        model.addAttribute("product", product);
+
+        return "addProduct";
+    }
+
+    @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
+    public String addProductPost(@ModelAttribute("product") Product product) {
+        productDao.addProduct(product);
+
+        return "redirect:/admin/productInventory";
+    }
+}
